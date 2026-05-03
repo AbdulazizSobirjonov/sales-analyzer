91974905fdc31b795831dfbe703e1df6f749c02e
AbdulazizSobirjonov <lazizsabirjanov@gmail.com>
2026-05-03 14:47:07 +0500

    Add sales data analyzer using Streamlit and panda

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+
+st.title("📊 Sales Data Analyzer")
+
+file = st.file_uploader("CSV yoki Excel yuklang", type=["csv","xlsx"])
+
+if file:
+    df = pd.read_csv(file) if file.name.endswith("csv") else pd.read_excel(file)
+    st.dataframe(df)
+    st.write(df.describe())
